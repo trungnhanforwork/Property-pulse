e9e05d97f02d3a7dfa96fa1524fe718c9c7762b2
trungnhan <trungnhanforwork@gmail.com>
2024-03-30 00:18:05 +0700

    feat(navbar): navbar links, dropdowns, activelinks, render condition

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPluse",
@@ -11,7 +12,8 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar></Navbar>
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const PropertiesPage = () => {
+  return <div>Property</div>;
+};
+
+export default PropertiesPage;
